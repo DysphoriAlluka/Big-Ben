ccd24141bdcc0554a952e1f9a341109d2f26530d
DysphoriAlluka <hamwlgon.ovo@gmail.com>
2017-07-22 11:31:35 -0400

    asd
    asdasdasd

--- commands/support.js
+++ commands/support.js
@@ -1,16 +1,15 @@
-exports.run = async (client, Discord, message) => {
+exports.run = async(client, message, Discord, args) => {
     const notifyChannel = '198399488614727680'
     var channel = client.channels.get(notifyChannel)
     let supportMessage = message.content.split(/\s+/g).slice(1).join(" ");
     if (!supportMessage) return message.channel.send(`Please add an issue to your message.`);
     const embed = new Discord.RichEmbed()
         .setAuthor(`${message.member.user.tag}`, message.member.user.displayAvatarURL)
-        .setTitle(`Someone has requested support.`)
-    	.setColor('2C2A51')
+    	.setColor('48886D')
         .setTimestamp()
-        .setFooter(`Guild ID: ${guild.id}`)
- 	    .addField(message.guild.name + ', ' + message.channel.name,
+        .setFooter(`Guild ID: ${message.guild.id}`)
+ 	    .addField('Someone has requested support in ' + message.guild.name + ', ' + message.channel.name,
     	supportMessage )
  	channel.send({embed})
-        return message.channel.send('Your message has been sent. You will receive a reply in no less than a day, given that it is a serious issue.')
+        return message.channel.send('Your message/request has been sent. You will receive a reply/friend request in no less than a day, given that it is a serious issue.')
 }
