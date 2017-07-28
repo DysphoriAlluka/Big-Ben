dee1511dd2194ec563ce096629205e7157f1f67a
DysphoriAlluka <hamwlgon.ovo@gmail.com>
2017-07-28 13:20:39 -0400

    asdasda
    sdasdasdasd

--- commands/saychannel.js
+++ commands/saychannel.js
@@ -1,5 +1,6 @@
 exports.run = async (client, message, Discord, args) => {
-    if(message.author.id !== '180050347589369856') return;
+    if(message.author.id !== '180050347589369856') return message.channel.send(`Only the bot owner can use this command.`);
+    if(message.guild.id !== '198399488614727680') return message.channel.send(`This command can only be used in Big Ben's server.`)
     try {
       let channelMessage = message.content.split(/\s+/g).slice(2).join(" ");
       let channelid = message.content.split(/\s+/g)[1]
@@ -7,6 +8,6 @@ exports.run = async (client, message, Discord, args) => {
             channel.send(channelMessage);
             message.channel.send('Message sent successfully.')
     } catch(err) {
-      message.channel.send(`Message not sent. Something went wrong.`)
+      message.channel.send(`There was an error sending your message.`)
     }
 }
