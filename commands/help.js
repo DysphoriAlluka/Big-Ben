4fb7640ff487985305997d3ea8afa52e6e4c1bec
DysphoriAlluka <hamwlgon.ovo@gmail.com>
2017-07-28 17:49:15 -0400

    asdf
    sadfasdfasdf

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ exports.run = async (client, message, Discord, args) => {
         .setThumbnail(client.user.displayAvatarURL)
 	    .setFooter(`Any message from the bot can be removed by reacting with a ⏲ emoji.`)
         .setDescription('Hi! My name is Ben and I\'m a simple bot created to say bong every hour in UTC! However, I\'ve grown to have more commands! Ben aims to have commands that not a lot of bots have! You can also use --help [command] to get help on a specific command!')
-        .addField(`__Status:__`, `Online | Currently still in development, updates are added constantly!` )
+        .addField(`__Status:__`, `Online | Currently still in development` )
         .addField(`__Invite Link:__`, `https://goo.gl/rpihva`)
         .addField(`__Commands:__`, `My prefix is --\n You can find a list of my commands as of now here:\n https://hastebin.com/ehugejodev.md `)
         .addField(`__Support:__`, `You can join this server https://discord.gg/zE9ZSQz to be imprisoned until I get to you.\n Or use --support [message] to send a support message.`)
