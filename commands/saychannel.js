5a217ea436ec00f25f54bed963aaa8c9cd5c3336
DysphoriAlluka <hamwlgon.ovo@gmail.com>
2017-07-28 13:18:01 -0400

    sdfasdfasdf
    asdasdf

--- commands/saychannel.js
+++ commands/saychannel.js
@@ -1,7 +1,12 @@
 exports.run = async (client, message, Discord, args) => {
     if(message.author.id !== '180050347589369856') return;
+    try {
       let channelMessage = message.content.split(/\s+/g).slice(2).join(" ");
       let channelid = message.content.split(/\s+/g)[1]
         let channel = client.channels.get(`${channelid}`)
             channel.send(channelMessage);
+            message.channel.send('Message sent successfully.')
+    } catch(err) {
+      message.channel.send(`Message not sent. Something went wrong.`)
+    }
 }
