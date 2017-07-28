dca5e8d51fd39d009cb1121efbf6d5010d3cb9cc
DysphoriAlluka <hamwlgon.ovo@gmail.com>
2017-07-27 21:36:02 -0400

    ad
    asdasdasd

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ exports.run = async (client, message, Discord) => {
     .addField(`__Status:__`, `Online | Currently still in development` )
     .addField(`__Invite Link:__`, `https://goo.gl/rpihva`)
     .addField(`__Commands:__`, `My prefix is -- \n You can find a list of my commands as of now here: \n   https://hastebin.com/duxeduqiwa.md `)
-    .addField(`__Support:__`, `You can join this server https://discord.gg/zE9ZSQz to be imprisoned until i get to you. \n Or use --support [message] to send a support message.`)
+    .addField(`__Support:__`, `DM <@180050347589369856> for help. \n You can join this server https://discord.gg/zE9ZSQz to be imprisoned until i get to you. \n  Or use --support [message] to send a support message.`)
     .addField(`__Additional Info:__`, `This bot is based purely on Voctor's original Big Ben bot. Seeing it offline all the time was devastating, to say the least. Hence the creation of this bootleg, deepfried, Ben. All credits go to where it's due.`, true);
  	message.channel.send({embed}).then(m=>m.react("⏲"))
 }
