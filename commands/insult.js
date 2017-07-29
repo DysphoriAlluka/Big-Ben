3c018eb832f287dc53c688fcbf876ac8ff5cfe11
DysphoriAlluka <hamwlgon.ovo@gmail.com>
2017-07-29 11:50:24 -0400

    adasd
    asdasda

--- commands/insult.js
+++ commands/insult.js
@@ -1,5 +1,9 @@
 exports.run = async (client, message) => {
-    message.delete().catch(O_o=>{}); 
-      message.channel.send(`${client.consts.insults[Math.round(Math.random() * (client.consts.insults.length - 1))]}`
-  )
+    if(message.author.id === '180050347589369856') {
+      message.delete().catch(O_o=>{}); 
+      return message.channel.send(`${client.consts.insults[Math.round(Math.random() * (client.consts.insults.length - 1))]}`)
+    }
+    if(message.author.id !== '180050347589369856') {
+      return message.channel.send(`${client.consts.insults[Math.round(Math.random() * (client.consts.insults.length - 1))]}`)
+    }
 }
