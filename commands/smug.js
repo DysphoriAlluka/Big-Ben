be366b967299f9aeea62f2da524945c6a544f12f
DysphoriAlluka <hamwlgon.ovo@gmail.com>
2017-07-29 21:55:06 -0400

    ytff
    yvytfty

--- commands/smug.js
+++ commands/smug.js
@@ -2,5 +2,6 @@ exports.run = async (client, message, Discord) => {
     var randomNumber = Math.floor(Math.random() * 58) + 1  
     const embed = new Discord.RichEmbed()
         .setImage(`http://smug.moe/smg/${randomNumber}.png`)
+        .setColour('#9D9DBD')
     message.channel.send({embed})
 }
