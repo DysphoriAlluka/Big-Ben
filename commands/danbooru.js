d4115d5a0e6368d2fe61bd016bdb42e832c0fe80
DysphoriAlluka <hamwlgon.ovo@gmail.com>
2017-07-31 21:15:43 -0400

    hj
    hjkjhhjk

--- commands/danbooru.js
+++ commands/danbooru.js
@@ -1,16 +1,11 @@
-const Danbooru = require('danbooru')
-let booru = new Danbooru()
+const Kaori = require('kaori');
+const kaori = new Kaori();
 
-exports.run = async (client, message, Discord, args) => {
-    if(!message.channel.nsfw && message.author.id != '180050347589369856') return message.channel.send('This is not an NSFW channel!')
-    var query = message.content.split(/\s+/g).slice(1).join(" ");
-    message.channel.send(`Searching for **${query}** on Danbooru!`).then(m => m.delete(3000));
-    booru.posts(query).then(async post => {
-     let postsId = String(post)
-     var id = postsId.split(',')[Math.floor(Math.random() * 20)]
-        if(!id) {
-            return message.channel.send(`There were no results for **${query}**!`)
-        }
-    return message.channel.send(`https://danbooru.donmai.us/posts/${id}`)
-    })
+exports.run = (client, message, Discord, args) => {
+    if(!message.channel.nsfw && message.author.id != '180050347589369856') return message.channel.send('This is not an NSFW channel!');
+     var query = message.content.split(/\s+/g).slice(1).join(" ");
+     message.channel.send(`Searching for **${query}** on Danbooru!`).then(m => m.delete(3000));
+        kaori.search('danbooru', { tags: [query], limit: 1, random: true })
+         .then(images => message.channel.send(":card_box: " + query + '\n' + images[0].common.fileURL))
+         .catch(err => message.channel.send(`No results found for **${query}**`));
 }
