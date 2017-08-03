684ac6efe1928931c0ee15a3686583ef2b414aec
DysphoriAlluka <hamwlgon.ovo@gmail.com>
2017-08-03 18:09:17 -0400

    now shows avatar as icon instead of ugly f emoji icon

--- commands/f.js
+++ commands/f.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, Discord) => {
     const embed = new Discord.RichEmbed()
-        .setAuthor(`${message.author.username} has paid their respects.`, 'https://a.safe.moe/TGUHH.png')
+        .setAuthor(`${message.author.username} has paid their respects.`, message.member.user.displayAvatarURL)
         .setColor('#4E373B')
         .setFooter(`Press F to pay your respects.`)
     message.channel.send({embed}).then(m=>m.react("🇫"))
