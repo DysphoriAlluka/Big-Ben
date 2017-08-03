225fb70de140de4e189ad5187cd4a613d6d116af
DysphoriAlluka <hamwlgon.ovo@gmail.com>
2017-08-03 18:54:08 -0400

    bolded some text wow

--- commands/f.js
+++ commands/f.js
@@ -1,7 +1,7 @@
-exports.run = async (client, message, Discord) => {
+exports.run = (client, message, Discord) => {
     const embed = new Discord.RichEmbed()
-        .setAuthor(`${message.author.username} has paid their respects.`, message.member.user.displayAvatarURL)
+        .setAuthor(`${message.author.username} has paid their respects.`, 'https://a.safe.moe/TGUHH.png')
         .setColor('#4E373B')
-        .setFooter(`Press F to pay your respects.`)
+        .setFooter(`Press F to pay 𝐲𝐨𝐮𝐫 respects.`)
     message.channel.send({embed}).then(m=>m.react("🇫"))
 }
