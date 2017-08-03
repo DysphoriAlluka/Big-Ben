265ef325bd04771ca65adb2aa8e722bed4ab1656
DysphoriAlluka <hamwlgon.ovo@gmail.com>
2017-08-03 19:55:18 -0400

    fixed f again

--- commands/f.js
+++ commands/f.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, Discord) => {
     const embed = new Discord.RichEmbed()
-        .setAuthor(`${message.author.username} has paid their respects.`, 'https://a.safe.moe/TGUHH.png')
+        .setAuthor(`${message.author.username} has paid their respects.`, message.author.displayAvatarURL)
         .setColor('#4E373B')
         .setFooter(`Press F to pay 𝐲𝐨𝐮𝐫 respects.`)
     message.channel.send({embed}).then(m=>m.react("🇫"))
