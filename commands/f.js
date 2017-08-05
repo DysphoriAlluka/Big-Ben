be51877c06319e9ba4883d33d276f97894bf1df1
DysphoriAlluka <hamwlgon.ovo@gmail.com>
2017-08-05 16:32:06 -0400

    respect machine updated

--- commands/f.js
+++ commands/f.js
@@ -3,14 +3,14 @@ exports.run = async (client, message, Discord, args) => {
     const embed = new Discord.RichEmbed()
         .setAuthor(`${message.author.username} has paid their respects.`, message.author.displayAvatarURL)
         .setColor('#4E373B')
-        .setFooter(`Press F to pay 𝐲𝐨𝐮𝐫 respects.`)
+        .setFooter(`Press F to pay your respects.`)
     return message.channel.send({embed}).then(m=>m.react("🇫"))
     } else {
         const respect = message.content.split(/\s+/g).slice(1).join(" ");
         const embed = new Discord.RichEmbed()
             .setAuthor(`${message.author.username} has paid their respects to ${respect}.`, message.author.displayAvatarURL)
             .setColor('#4E373B')
-            .setFooter(`Press F to pay 𝐲𝐨𝐮𝐫 respects.`)
+            .setFooter(`Press F to pay your respects.`)
         return message.channel.send({embed}).then(m=>m.react("🇫"))
     }
 }
