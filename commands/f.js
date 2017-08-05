db2e1f6ba8018cf3b81e66adefb9073faf681e9c
DysphoriAlluka <hamwlgon.ovo@gmail.com>
2017-08-05 16:06:59 -0400

    i declared respect and i put args in the author field smh how stupid can someone be

--- commands/f.js
+++ commands/f.js
@@ -6,9 +6,9 @@ exports.run = async (client, message, Discord, args) => {
         .setFooter(`Press F to pay 𝐲𝐨𝐮𝐫 respects.`)
     return message.channel.send({embed}).then(m=>m.react("🇫"))
     } else {
-        const respect = message.content.split(/\s+/g).slice(1).join(" ");
+        const respect = args.join(" ");
         const embed = new Discord.RichEmbed()
-            .setAuthor(`${message.author.username} has paid their respects to ${args}.`, message.author.displayAvatarURL)
+            .setAuthor(`${message.author.username} has paid their respects to ${respect}.`, message.author.displayAvatarURL)
             .setColor('#4E373B')
             .setFooter(`Press F to pay 𝐲𝐨𝐮𝐫 respects.`)
         return message.channel.send({embed}).then(m=>m.react("🇫"))
